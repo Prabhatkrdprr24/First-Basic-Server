a62cb3bc29972de54bd2989847d4ccff8b46f3d9
prabhatkrdprr24 <prabhatkrdprr24@gmail.com>
2025-02-05 02:15:46 +0530

    core node js code completed

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const http = require('http');
 //Local module
 const { handler } = require('./RequestHandler');
 
+function compare(num){
+    if(num = 10){
+        return "Equal";
+    }
+    else{
+        return "Not Equal";
+    }
+}
+
 const server = http.createServer(handler);
 const PORT = 3001;
 server.listen(PORT, () => {
