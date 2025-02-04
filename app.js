449c19d11cde0eaf655b75b3d3c23bb60a97df1e
prabhatkrdprr24 <prabhatkrdprr24@gmail.com>
2025-02-04 19:12:47 +0530

    reading chunk code added

--- app.js
+++ app.js
@@ -1,22 +1,40 @@
 const http = require('http');
+const fs = require('fs');
 
 console.log("I was here");
 
 const requestHandler = (req, res) => {
-    console.log("Request received", req.url, req.method, req.headers);
+    console.log("Request received", req.url, req.method);
     res.setHeader('Content-Type', 'text/html');
     if(req.url === '/'){
         res.write(`
             <!DOCTYPE html>
             <html lang="en">
-                <head>
-                    <title>Node js</title>
-                </head>
-                <body>
-                    <h1>Welcome to first server</h1>
-                </body>
+            <head>
+                <title>Myntra</title>
+            </head>
+            <body>
+                <h1>Welcome to first server</h1>
+                <form action="/buy-product" method="POST">
+                    <input type="text" name="product" placeholder="Enter product name">
+                    <br>
+                    <input type="text" name="budget" placeholder="Enter your budget">
+                    <input type="submit">
+                </form>
+            </body>
             </html>
         `);
+    }
+    else if(req.url === '/buy-product'){
+        console.log("form data received");
+
+        req.on('data', (chunk) => {
+            console.log(chunk);
+        });
+        
+        fs.writeFileSync('buy.txt', "Myntra app");
+        res.statusCode = 302;
+        res.setHeader('Location', '/products');
     }
     else if(req.url === '/products'){
         res.write(`
@@ -32,6 +50,7 @@ const requestHandler = (req, res) => {
         `);
     }
     else{
+        res.statusCode = 404;
         res.write(`
             <!DOCTYPE html>
             <html lang="en">
