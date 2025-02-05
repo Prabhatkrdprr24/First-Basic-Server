f73c5087acbdd5a2d89b926c2ad540df60c50d92
prabhatkrdprr24 <prabhatkrdprr24@gmail.com>
2025-02-05 10:43:22 +0530

    handling routes code added

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
-//core module
-const http = require('http');
-
 //External module
 const express = require('express');
 
 const app = express();
 
 //first middleware
-app.use((req, res, next) => {
+app.use("/", (req, res, next) => {
     console.log("Middleware 1", req.url, req.method);
     next(); 
 });
 
 //second middleware
-app.use((req, res, next) => {
+app.use("/test", (req, res, next) => {
     console.log("Middleware 2", req.url, req.method);
-    res.send("<h1>Hello from express</h1>");
+    res.send("<h1>Hello from express dprr</h1>");
 });
 
-const server = http.createServer(app);
 const PORT = 3001;
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`server running at http://localhost:${PORT}/`)
 });
